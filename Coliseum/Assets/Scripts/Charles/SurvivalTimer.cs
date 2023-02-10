7ade4c4cedbd9aca2a2d1ed05cdb108895c351b3
DrPeachy <p1067838263@gmail.com>
2023-02-10 01:47:14 -0500

    timer

--- Coliseum/Assets/Scripts/Charles/SurvivalTimer.cs
+++ Coliseum/Assets/Scripts/Charles/SurvivalTimer.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using TMPro;
 
 public class SurvivalTimer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private float maxTime = 30;
+    [SerializeField] private float curTime;
+    public TextMeshProUGUI timer;
+    bool isOn = false;
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.R)){
+            startTimer();
+        }
+
+        if(isOn){
+            timer.text = $"{Mathf.Round(curTime)}:{Mathf.Round(curTime * 100f) % 100}";
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void startTimer(){
+
+
+        Sequence sq = DOTween.Sequence();
+        sq
+        .SetId("timer")
+        .OnStart(()=>{
+            isOn = true;
+            curTime = maxTime;
+            DOTween
+            .To(()=>curTime, x=>curTime = x, 0f, 30f)
+            .SetEase(Ease.OutSine);
+            
+
+        })
+        .AppendInterval(15f)
+        .Append(
+            timer
+            .DOColor(Color.red, 16f)
+            .SetEase(Ease.InCubic)
+        )
+        .Join(
+            timer.rectTransform
+            .DOShakeAnchorPos(
+                duration: 16f,
+                strength: 20,
+                vibrato: 50
+            )
+            .SetEase(Ease.InOutCubic)
+        )
+        .OnComplete(()=>{ isOn = false; });
+    
     }
 }
