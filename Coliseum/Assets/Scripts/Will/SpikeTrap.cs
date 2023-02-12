5274a267d863dcde2a0206e9b22af9cc7a798726
William Yang <wy2037@nyu.edu>
2023-02-12 12:39:40 -0500

    Spike Trap + Deleted unused enemy script

---
+++ Coliseum/Assets/Scripts/Will/SpikeTrap.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrap : MonoBehaviour
+{
+    public Animator spriteAnimator;
+    [SerializeField]
+    float damage, time, maxInterval;
+    float interval;
+
+    /*
+    Summary:
+    Spike has a set timer when it activates and deactivates.
+    When it activates, it set triggers of activate which will turn on box collider trigger.
+    The time interval will also be reset to full and wait for the next activation time.
+    */
+    void Start() {
+        interval = maxInterval;
+    }
+
+    void Update() {
+        if (interval >= 0) {
+            interval -= Time.deltaTime;
+        } else {
+            spriteAnimator.SetTrigger("Activate");
+            Invoke("setMaxInterval", 1f); // can adjust this number
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            other.GetComponent<PlayerController>().TakeDamage(damage);
+            other.GetComponent<PlayerController>().TakeKnockback(new Vector2(Random.Range(-1,1), Random.Range(-1,1)), time);
+        }
+    }
+
+    void setMaxInterval() {
+        interval = maxInterval;
+    }
+}
