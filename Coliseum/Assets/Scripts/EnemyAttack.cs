153885b99049e69844aef6116555bf9ff297e959
TaneimMiah <tsm6910@nyu.edu>
2023-02-13 11:42:02 -0500

    Enemy attack scaling works too now probably

--- Coliseum/Assets/Scripts/EnemyAttack.cs
+++ Coliseum/Assets/Scripts/EnemyAttack.cs
@@ -5,19 +5,16 @@ using UnityEngine;
 public class EnemyAttack : MonoBehaviour
 {
     public MeleeEnemy enemy;
-    float damage, knockback;
 
     void Start() {
-        damage = enemy.damage;
-        knockback = enemy.knockback;
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player") && !enemy.hasHit)
         {
-            PlayerController.p.TakeKnockback(enemy.direction, knockback);
-            PlayerController.p.TakeDamage(damage);
+            PlayerController.p.TakeKnockback(enemy.direction, enemy.knockback);
+            PlayerController.p.TakeDamage(enemy.damage);
             enemy.hasHit = true;
         }
     }
